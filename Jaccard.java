9aaf46084bd64f01d79afd31e352c983ba7e2e5f
arup-cs <arup.apee@gmail.com>
2017-12-29 00:43:24 -0600

    Added the Lists of Double

--- Jaccard.java
+++ Jaccard.java
@@ -1,10 +1,28 @@
 import java.io.*;
+import java.util.*;
 
+//This program computes the Jaccard distance between two sets
 public class Jaccard{
-		//static String name="Arup";
-	public static void main(String[] args){
 		
-		System.out.println("My Name is : "+name);
+	public static void main(String[] args){
+		ArrayList<Double> set1;
+		ArrayList<Double> set2;
+		
+		set1=new ArrayList<Double>();
+		set2=new ArrayList<Double>();
+	
+		set1.add(1.1);
+		set1.add(2.2); 
+		set1.add(3.3); 
+		set1.add(4.4);
+
+		set2.add(0.1); 
+		set2.add(0.2); 
+		set2.add(4.4); 
+		set2.add(0.3);
+
+
+		System.out.println("Program is running successefully");
 	}
 
 
