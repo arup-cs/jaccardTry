0fa02614282829c6d8553e7a010b365b7238a565
arup-cs <arup.apee@gmail.com>
2017-12-29 01:09:32 -0600

    Union and Intersection implemented

--- Jaccard.java
+++ Jaccard.java
@@ -21,8 +21,25 @@ public class Jaccard{
 		set2.add(4.4); 
 		set2.add(0.3);
 
+		Set<Double> dSet1=new HashSet<>(set1);
+		Set<Double> dSet2=new HashSet<>(set2);
+		//This is the destructive version of union
+		//It will destroy the dSet1's all value
+	//	dSet1.addAll(dSet2);
+		
+		
+		//This is the non-destructive version of union
+		//It will not destroy, because we are copying the list first
 
-		System.out.println("Program is running successefully");
+		Set<Double> union=new HashSet<>(set1);
+		union.addAll(dSet2);
+		System.out.println("Union set has "+union.size()+" elements ");
+		
+		//This is non-destructuve version of intersection
+		Set<Double> intersection=new HashSet<>(set1);
+		intersection.retainAll(dSet2);
+		System.out.println("Intersection set has "+intersection.size()+" elements ");
+	
 	}
 
 
