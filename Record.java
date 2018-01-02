e379215d794a805e7b163587f564ac2e7316c033
arup-cs <arup.apee@gmail.com>
2018-01-02 15:33:04 -0600

    TrainTestRecord placed together

---
+++ Record.java
@@ -0,0 +1,56 @@
+
+public class Record {
+	float sepalL;
+	float sepalW;
+	float petalL;
+	float petalW;
+	String category;
+	
+	public Record(){
+
+	}
+	public float getSepalL() {
+		return sepalL;
+	}
+
+	public void setSepalL(float sepalL) {
+		this.sepalL = sepalL;
+	}
+
+	public float getSepalW() {
+		return sepalW;
+	}
+
+	public void setSepalW(float sepalW) {
+		this.sepalW = sepalW;
+	}
+
+	public float getPetalL() {
+		return petalL;
+	}
+
+	public void setPetalL(float petalL) {
+		this.petalL = petalL;
+	}
+
+	public float getPetalW() {
+		return petalW;
+	}
+
+	public void setPetalW(float petalW) {
+		this.petalW = petalW;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	
+
+	
+
+}
